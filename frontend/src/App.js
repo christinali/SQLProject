e8812055c795eadbbcbe821c2e020e615679157d
Matthew O'Boyle <oboyle.matthew@duke.edu>
2018-11-08 00:49:54 -0500

    Changed frontend to read from 0.0.0.0

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,10 +4,20 @@ import './App.css';
 
 class App extends Component {
   render() {
+      var testing = new Promise((resolve, reject) => {
+          fetch("https://0.0.0:5000", {
+              method: 'GET',
+              headers: opts.additionalHeaders || {},
+          })
+              .then(res => res.json())
+              .then(json => {console.log(json); resolve(json)})
+              .catch(err => reject(err));
+      });
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
+            <p>{testing}</p>
           <p>
             Edit <code>src/App.js</code> and save to reload.
           </p>

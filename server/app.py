845d7b56953aed7ef6fb1138943451b90132a35a
Matthew O'Boyle <oboyle.matthew@duke.edu>
2018-11-30 15:59:54 -0500

    Changes to server

--- server/app.py
+++ server/app.py
@@ -1,46 +1,30 @@
-from flask import Flask, render_template, redirect, url_for
-from flask_sqlalchemy import SQLAlchemy
-import models
-import forms
+from flask import Flask
+from flask_restful import Api, Resource, reqparse
+from flask_cors import CORS
+
 
 app = Flask(__name__)
-app.secret_key = 's3cr3t'
-app.config.from_object('config')
-db = SQLAlchemy(app, session_options={'autocommit': False})
+api = Api(app)
+CORS(app)
 
-@app.route('/')
-def all_drinkers():
-    drinkers = db.session.query(models.Drinker).all()
-    return render_template('all-drinkers.html', drinkers=drinkers)
 
-@app.route('/drinker/<name>')
-def drinker(name):
-    drinker = db.session.query(models.Drinker)\
-        .filter(models.Drinker.name == name).one()
-    return render_template('drinker.html', drinker=drinker)
+users = [
+    {
+        "name": "Matt",
+        "age": 20
+    },
+    {
+        "name": "Christina",
+        "age": 19
+    }
+]
 
-@app.route('/edit-drinker/<name>', methods=['GET', 'POST'])
-def edit_drinker(name):
-    drinker = db.session.query(models.Drinker)\
-        .filter(models.Drinker.name == name).one()
-    beers = db.session.query(models.Beer).all()
-    bars = db.session.query(models.Bar).all()
-    form = forms.DrinkerEditFormFactory.form(drinker, beers, bars)
-    if form.validate_on_submit():
-        try:
-            form.errors.pop('database', None)
-            models.Drinker.edit(name, form.name.data, form.address.data,
-                                form.get_beers_liked(), form.get_bars_frequented())
-            return redirect(url_for('drinker', name=form.name.data))
-        except BaseException as e:
-            form.errors['database'] = str(e)
-            return render_template('edit-drinker.html', drinker=drinker, form=form)
-    else:
-        return render_template('edit-drinker.html', drinker=drinker, form=form)
-
-@app.template_filter('pluralize')
-def pluralize(number, singular='', plural='s'):
-    return singular if number in (0, 1) else plural
-
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+class User(Resource):
+    def get(self, name):
+        for user in users:
+            if (name == user["name"]):
+                return user, 200
+        return "User not found", 404
+        
+api.add_resource(User, "/user/<string:name>")
+app.run(debug=True)

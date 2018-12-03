0add83a3cb223bc3b453fce4fdadb6c9953963b6
Matthew O'Boyle <oboyle.matthew@duke.edu>
2018-12-03 00:38:48 -0500

    Login

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,11 +2,18 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Timeline from './scenes/Timeline';
+import Login from './scenes/Login';
 
 class App extends Component {
+    state = {login: false}
+
+
   render() {
     return (
-      <Timeline/>
+        <div>
+            {this.state.login && <button style={{position: 'fixed', left: 0, top: 0}} onClick={() => this.setState({login: false})}>Log out</button>}
+            {this.state.login ? <Timeline/> : <Login login={() => this.setState({login: true})}/>}
+        </div>
     );
   }
 }
---
+++ frontend/src/scenes/Login.js
@@ -0,0 +1,72 @@
+import React from 'react';
+import firebase from 'firebase';
+
+const firebaseConfig = {
+    apiKey: "AIzaSyDfXgvgX2_eyPam6O3eenzLTJHrwHc2tdc",
+    authDomain: "sqlproject-b318c.firebaseapp.com",
+    databaseURL: "https://sqlproject-b318c.firebaseio.com",
+    projectId: "sqlproject-b318c",
+    storageBucket: "sqlproject-b318c.appspot.com",
+    messagingSenderId: "612693141535"
+};
+
+
+const app = firebase.initializeApp(firebaseConfig);
+
+export default class Login extends React.Component {
+    state = { email: '', password: '' };
+
+    handleLogin = () => {
+    const { email, password } = this.state;
+    app
+    .auth()
+    .signInWithEmailAndPassword(email, password)
+    .then(async user => {
+    if (user) {
+        this.props.login()
+        alert("You logged in successfully")
+    }
+})
+.catch(e => {
+    alert(e.message);
+    console.log(e)
+});
+};
+
+handleSignup = () => {
+    const { email, password } = this.state;
+    app
+        .auth()
+        .createUserWithEmailAndPassword(email, password)
+        .then(async user => {
+        if (user) {
+            this.props.login()
+            alert("You signed up successfully")
+        }
+    })
+.catch(e => {
+    alert(e.message);
+    console.log(e);
+});
+};
+
+render = () => {
+    const { email, password } = this.state;
+    return (
+        <div>
+<label>
+    Email:
+<input type="text" value={this.state.email} onChange={e => this.setState({email: e.target.value})} />
+    </label>
+    <label>
+    Password:
+<input type="text" value={this.state.password} onChange={e => this.setState({password: e.target.value})} />
+    </label>
+    <button onClick={this.handleLogin}>Log in</button>
+    <button onClick={this.handleSignup}>Sign up</button>
+
+    </div>
+    );
+};
+}
+

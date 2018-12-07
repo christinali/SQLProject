7a2d5fa1f5b79a70253297ca8a21a1ed3bbfcaf8
Christina Li <cjl56@duke.edu>
2018-12-07 16:50:35 -0500

    if u know, u know

--- ilovematt.sh
+++ ilovematt.sh
@@ -1,5 +1,6 @@
 dropdb bookbagging; createdb bookbagging; psql bookbagging -af create.sql
-psql bookbagging -af department.sql
+psql bookbagging -af department.sql; psql bookbagging -af professor.sql; psql bookbagging -af comment.sql
 
 #after this you should run app.py in the server folder
 #right now only the the /departments route is set up, but as we add more x.sql files more routes should become available
+
---
+++ professor.sql
@@ -0,0 +1,8 @@
+INSERT INTO Professor VALUES
+('Matthew OBoyle',0),
+('Suchir Bhatt',1),
+('Salil Mitra',2),
+('Christina Li',3),
+('Feroze Mohideen',4),
+('Shreya Shankar',5),
+('Tiff Jiang',6);

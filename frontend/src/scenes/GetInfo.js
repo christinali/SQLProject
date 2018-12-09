c4fefd29c1ed76bb5c2022cddb7e821c3e7f3cc3
Salil Mitra <salil.mitra11@gmail.com>
2018-12-09 01:04:03 -0500

    GOAT commit never reverse

--- frontend/src/scenes/GetInfo.js
+++ frontend/src/scenes/GetInfo.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import axios from 'axios';
 import Select from 'react-select';
+import StarRatings from 'react-star-ratings';
 
 
 class GetInfo extends React.Component {
@@ -8,6 +9,7 @@ class GetInfo extends React.Component {
         super();
         this.state = {
             id: null, //whatever the user login id is
+            email: '',
             currClass: null,
             currProf: null,
             Treqs: null, //load with user ID
@@ -23,22 +25,10 @@ class GetInfo extends React.Component {
         this.getAllProfs();
     }
 
-    getClass() {
-        axios.get('http://localhost:5000/get-class-info?class_id=' + this.state.id)
-            .then(res => {
-                console.log(res.data);
-                this.setState({currClass: res.data});
-            })
-            .catch(e => console.log(e))
-    }
-
-    getProf() {
-        axios.get('http://localhost:5000/get-prof-info?prof_id=' + this.state.id)
-            .then(res => {
-                console.log(res.data);
-                this.setState({currProf: res.data});
-            })
-            .catch(e => console.log(e))
+    componentDidMount() {
+        this.setState({
+            email : this.props.email,
+        })
     }
 
     getTReqs() {
@@ -85,59 +75,61 @@ class GetInfo extends React.Component {
 
     splitTreqs(array) {
       var res = "";
-      for (var i = 0; i < array.length; i++) {
-        res = res + " " + array[i];
+      if (array) {
+          for (var i = 0; i < array.length; i++) {
+            res = res + " " + array[i].toUpperCase();
+          }
       }
       return res;
     }
 
     peruse(result) {
         if (this.state.searchCat === 'classname') {
-          let resultCopy = this.state.allClasses;
-          resultCopy = this.state.allClasses.filter(c => {
-            return c.name.toLowerCase().includes(result.target.value.toLowerCase());
-          })
-          this.setState({
-            query: result.target.value,
-            resultOut: resultCopy,
-          })
+            let resultCopy = this.state.allClasses;
+            resultCopy = this.state.allClasses.filter(c => {
+              return c.name.toLowerCase().includes(result.target.value.toLowerCase());
+            })
+            this.setState({
+              query: result.target.value,
+              resultOut: resultCopy,
+            })
         }
         else if (this.state.searchCat === 'prof') {
-          let resultCopy = this.state.allProfs;
-          resultCopy = this.state.allProfs.filter(c => {
-            return c.name.toLowerCase().includes(result.target.value.toLowerCase());
-          })
-          this.setState({
-            query: result.target.value,
-            resultOut: resultCopy,
-          })
+            let resultCopy = this.state.allProfs;
+            resultCopy = this.state.allProfs.filter(c => {
+              return c.name.toLowerCase().includes(result.target.value.toLowerCase());
+            })
+            this.setState({
+              query: result.target.value,
+              resultOut: resultCopy,
+            })
         }
         else if (this.state.searchCat === 'classid') {
-          let resultCopy = this.state.allClasses;
-          resultCopy = this.state.allClasses.filter(c => {
-            if (c.dept && c.num) {
-              let temp = c.dept.toLowerCase() + c.num;
-              return temp.includes(result.target.value.toLowerCase());
-            }
-          })
-          this.setState({
-            query: result.target.value,
-            resultOut: resultCopy,
-          })
+            let resultCopy = this.state.allClasses;
+            resultCopy = this.state.allClasses.filter(c => {
+              if (c.dept && c.num) {
+                let temp = c.dept.toLowerCase() + c.num;
+                return temp.includes(result.target.value.toLowerCase());
+              }
+            })
+            this.setState({
+              query: result.target.value,
+              resultOut: resultCopy,
+            })
         }
     }
 
     changeCat = (newValue: any) => {
-      if (newValue) {
-        this.setState ({
-          searchCat: newValue.value
-        })
-      }
-      else {
-        this.setState ({
-          searchCat: ''
-        })
-      }
+        if (newValue) {
+            this.setState ({
+              searchCat: newValue.value
+            })
+        }
+        else {
+            this.setState ({
+              searchCat: ''
+            })
+        }
     }
 
     render() {
@@ -162,7 +154,7 @@ class GetInfo extends React.Component {
                   <button onClick={this.props.logout}>Log Out</button>
                 </div>
                 <label>id:
-                    <input type="text" value={this.state.email} onChange={e => this.setState({id: e.target.value})} />
+                    <input type="text" value={this.state.id} onChange={e => this.setState({id: e.target.value})} />
                 </label>
                 <button onClick={() => this.getTReqs()}>Get Trecs</button>
                 <button onClick={() => this.getMajors()}>Get Majors</button>
@@ -170,26 +162,65 @@ class GetInfo extends React.Component {
                 <button onClick={() => this.getMajors()}>Get Majors</button>
 
                 <div className = 'Top3Recs'>
-                    <h2> Top Treq Classes: </h2>
+                    <h1> Top Treq Classes: </h1>
                     <div className = 'ReqsAll'style={{display: 'flex', flexDirection: 'row'}}>
                         {Treqs && Treqs[0] && <div className = 'ReqsComp'>
-                            <h1> {Treqs[0].dept}{Treqs[0].num} - {Treqs[0].name} </h1>
-                            <h2> Overall: {Treqs[0].overall} </h2>
-                            <h2> Difficulty: {Treqs[0].difficulty} </h2>
+                            <h2> {Treqs[0].dept}{Treqs[0].num} - {Treqs[0].name} </h2>
+                            <br/>
+                            <h3> Overall:
+                                <StarRatings
+                                    rating={Treqs[0].overall}
+                                    starDimension="22px"
+                                    starSpacing="3px"
+                                />
+                            </h3>
+                            <h3> Difficulty:
+                                <StarRatings
+                                    rating={Treqs[0].difficulty}
+                                    starDimension="22px"
+                                    starSpacing="3px"
+                                />
+                            </h3>
                             <h4> Treqs Satisfied: {this.splitTreqs(Treqs[0].satisfiesNeeded)} </h4>
                         </div>}
 
                         {Treqs && Treqs[1] && <div className = 'ReqsComp'>
-                            <h1> {Treqs[1].dept}{Treqs[1].num} - {Treqs[1].name} </h1>
-                            <h2> Overall: {Treqs[1].overall} </h2>
-                            <h2> Difficulty: {Treqs[1].difficulty} </h2>
+                            <h2> {Treqs[1].dept}{Treqs[1].num} - {Treqs[1].name} </h2>
+                            <br/>
+                            <h3> Overall:
+                                <StarRatings
+                                    rating={Treqs[1].overall}
+                                    starDimension="22px"
+                                    starSpacing="3px"
+                                />
+                            </h3>
+                            <h3> Difficulty:
+                                <StarRatings
+                                    rating={Treqs[1].difficulty}
+                                    starDimension="22px"
+                                    starSpacing="3px"
+                                />
+                            </h3>
                             <h4> Treqs Satisfied: {this.splitTreqs(Treqs[1].satisfiesNeeded)} </h4>
                         </div>}
 
                         {Treqs && Treqs[2] && <div className = 'ReqsComp'>
-                            <h1> {Treqs[2].dept}{Treqs[2].num} - {Treqs[2].name} </h1>
-                            <h2> Overall: {Treqs[2].overall} </h2>
-                            <h2> Difficulty: {Treqs[2].difficulty} </h2>
+                            <h2> {Treqs[2].dept}{Treqs[2].num} - {Treqs[2].name} </h2>
+                            <br/>
+                            <h3> Overall:
+                                <StarRatings
+                                    rating={Treqs[2].overall}
+                                    starDimension="22px"
+                                    starSpacing="3px"
+                                />
+                            </h3>
+                            <h3> Difficulty:
+                                <StarRatings
+                                    rating={Treqs[2].difficulty}
+                                    starDimension="22px"
+                                    starSpacing="3px"
+                                />
+                            </h3>
                             <h4> Treqs Satisfied: {this.splitTreqs(Treqs[2].satisfiesNeeded)} </h4>
                         </div>}
                     </div>
@@ -223,24 +254,66 @@ class GetInfo extends React.Component {
                     </div>
                 </div>
                 <div className = 'Top3Recs'>
-                    <h2> Top Major Classes: </h2>
+                    <h1> Top Major Classes: </h1>
                     <div className = 'ReqsAll'style={{display: 'flex', flexDirection: 'row'}}>
                         {Majors && Majors[0] && <div className = 'ReqsComp'>
-                            <h1> {Majors[0].dept}{Majors[0].num} - {Treqs[0].name} </h1>
-                            <h2> Overall: {Majors[0].overall} </h2>
-                            <h2> Difficulty: {Majors[0].difficulty} </h2>
+                            <h2> {Majors[0].dept}{Majors[0].num} - {Majors[0].name} </h2>
+                            <br/>
+                            <h3> Overall:
+                                <StarRatings
+                                    rating={Majors[0].overall}
+                                    starDimension="22px"
+                                    starSpacing="3px"
+                                />
+                            </h3>
+                            <h3> Difficulty:
+                                <StarRatings
+                                    rating={Majors[0].difficulty}
+                                    starDimension="22px"
+                                    starSpacing="3px"
+                                />
+                            </h3>
+                            <h4> Treqs Satisfied: {this.splitTreqs(Majors[0].satisfiesNeeded)} </h4>
                         </div>}
 
                         {Majors && Majors[1] && <div className = 'ReqsComp'>
-                            <h1> {Majors[1].dept}{Majors[1].num} - {Majors[1].name} </h1>
-                            <h2> Overall: {Majors[1].overall} </h2>
-                            <h2> Difficulty: {Majors[1].difficulty} </h2>
+                            <h2> {Majors[1].dept}{Majors[1].num} - {Majors[1].name} </h2>
+                            <br/>
+                            <h3> Overall:
+                                <StarRatings
+                                    rating={Majors[1].overall}
+                                    starDimension="22px"
+                                    starSpacing="3px"
+                                />
+                            </h3>
+                            <h3> Difficulty:
+                                <StarRatings
+                                    rating={Majors[1].difficulty}
+                                    starDimension="22px"
+                                    starSpacing="3px"
+                                />
+                            </h3>
+                            <h4> Treqs Satisfied: {this.splitTreqs(Majors[1].satisfiesNeeded)} </h4>
                         </div>}
 
                         {Majors && Majors[2] && <div className = 'ReqsComp'>
-                            <h1> {Majors[2].dept}{Majors[2].num} - {Majors[2].name} </h1>
-                            <h2> Overall: {Majors[2].overall} </h2>
-                            <h2> Difficulty: {Majors[2].difficulty} </h2>
+                            <h2> {Majors[2].dept}{Majors[2].num} - {Majors[2].name} </h2>
+                            <br/>
+                            <h3> Overall:
+                                <StarRatings
+                                    rating={Majors[2].overall}
+                                    starDimension="22px"
+                                    starSpacing="3px"
+                                />
+                            </h3>
+                            <h3> Difficulty:
+                                <StarRatings
+                                    rating={Majors[2].difficulty}
+                                    starDimension="22px"
+                                    starSpacing="3px"
+                                />
+                            </h3>
+                            <h4> Treqs Satisfied: {this.splitTreqs(Majors[2].satisfiesNeeded)} </h4>
                         </div>}
                     </div>
                 </div>
@@ -252,6 +325,23 @@ class GetInfo extends React.Component {
 export default GetInfo;
 
 /*
+getClass() {
+    axios.get('http://localhost:5000/get-class-info?class_id=' + this.state.id)
+        .then(res => {
+            console.log(res.data);
+            this.setState({currClass: res.data});
+        })
+        .catch(e => console.log(e))
+}
+
+getProf() {
+    axios.get('http://localhost:5000/get-prof-info?prof_id=' + this.state.id)
+        .then(res => {
+            console.log(res.data);
+            this.setState({currProf: res.data});
+        })
+        .catch(e => console.log(e))
+}
 {currClass && <div>
     <h1>Class Info:</h1>
     {Object.keys(currClass).map(key => {

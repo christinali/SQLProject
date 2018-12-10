f2b4be627a6500e7db76c1db6c5b2d4292207c5f
Suchir Bhatt <sb503@duke.edu>
2018-12-09 21:42:25 -0500

    Data

--- server/prof_info.py
+++ server/prof_info.py
@@ -1,15 +1,15 @@
-class1 = {"id": "0", "name": "Compsci316 - Databases", "classNum": "CS316"}
+class1 = {"id": "0", "name": "Databases", "classNum": "316", "dept":"COMPSCI"}
 
-class2 = {"id": "1", "name": "Compsci201 - Algorithms", "classNum": "CS210"}
+class2 = {"id": "1", "name": "Data Structures and Algorithms", "classNum": "210", "dept":"COMPSCI"}
 
 comment1 = {"id": "0", "username": "Matthew O'Boyle", "up": 7, "down": 2, "comment": "Here's a comment", 
-"date": "2018-12-01", "overall": 4, "difficulty": 3.4, "grade": "B+","class":"Intro to Artificial Intelligence", "semester":"2014 Spring Term"} 
+"date": "2018-12-01", "overall": 4, "difficulty": 3.4, "grade": "B+","classNum":"CS270", "semester":"2014 Spring Term","class": [class1]} 
 comment2 = {"id": "1", "username": "Feroze", "up": 12, "down": 1, "comment": "Feroze's comment", 
-"date": "2018-11-29", "overall": 3.45, "difficulty": 1.4, "grade": "A-","class":"Data Structures and Algorithms", "semester":"2017 Fall Term"} 
+"date": "2018-11-29", "overall": 3.45, "difficulty": 1.4, "grade": "A-","classNum":"CS201", "semester":"2017 Fall Term","class": [class1]} 
 comment3 = {"id": "2", "username": "Christina", "up": 1, "down": 8, "comment": "Christina's comment", 
-"date": "2018-11-26", "overall": 3.7, "difficulty": 1.7, "grade": "A", "class":"Software Development", "semester":"2015 Fall Term"} 
+"date": "2018-11-26", "overall": 3.7, "difficulty": 1.7, "grade": "A", "class": [class1], "semester":"2015 Fall Term"} 
 comment4 = {"id": "3", "username": "Salil", "up": 47, "down": 3, "comment": "Salil's comment", 
-"date": "2018-11-21", "overall": 3.2, "difficulty": 3.45, "grade": "B-","class":"Software Development", "semester":"2018 Spring Term"} 
+"date": "2018-11-21", "overall": 3.2, "difficulty": 3.45, "grade": "B-","classNum":"CS308", "semester":"2018 Spring Term","class": [class1]} 
 
 
 jun = {"name": "Jun Yang", "id": 1, "overall": 4.7, "difficulty": 2.1, "nextSemClasses": [class1],

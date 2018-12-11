82cae2836729afdf8405136cc4e3fc8a7affcf8a
Salil Mitra <salil.mitra11@gmail.com>
2018-12-11 16:29:47 -0500

    fixed

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -41,11 +41,7 @@ class App extends Component {
   //2 = Sign Up
   //3 = ClassInfo
   //4 = ProfInfo
-<<<<<<< HEAD
-  state = {screen:2, email: '', currProf: null, currClass: null, headerText: '(Insert Title Here)'}
-=======
-  state = {screen:4, email: '', currProf: null, currClass: null, headerText: '(Insert Title Here)'}
->>>>>>>
+  state = {screen:1, email: '', currProf: null, currClass: null, headerText: '(Insert Title Here)'}
 
   render() {
     var name = 'main' + this.state.screen;

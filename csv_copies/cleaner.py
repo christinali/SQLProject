279fe0a81898df014f09d0522213ee66df62548b
Suchir Bhatt <sb503@duke.edu>
2018-12-10 23:17:11 -0500

    Algorithm Gods

--- csv_copies/cleaner.py
+++ csv_copies/cleaner.py
@@ -1,15 +1,19 @@
-filename = "sqlFiles/class.sql"
-filout = "classescleaned.sql"
+filename = "classes.csv"
+filout = "classescleaned.csv"
 
 with open(filename) as filein:
     with open(filout,"w") as fileo:
         for line in filein.readlines():
             lineSplit = line.split(",")
-            if len(lineSplit)>1:
-                print(lineSplit[2])
-            try:
-                int(lineSplit[2][1:-1])
-                if len(lineSplit)==16:
-                    fileo.write(line)
-            except:
-                print("nice")
+            # if len(lineSplit)>1:
+            #     print(lineSplit[2])
+            # try:
+            #     int(lineSplit[2][1:-1])
+            #     if len(lineSplit)==16:
+            #         fileo.write(line)
+            # except:
+            #     print("nice")
+            while len(lineSplit)!=15:
+                lineSplit[1] = lineSplit[0]+lineSplit[1]
+                lineSplit = lineSplit[1:]
+            fileo.write(",".join(lineSplit))

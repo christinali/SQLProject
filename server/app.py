fb0ec4f53973c6ef349afb9306698d8e56c8a2a3
Suchir Bhatt <sb503@duke.edu>
2018-12-12 13:47:32 -0500

    Treq algorithm optimizations

--- server/app.py
+++ server/app.py
@@ -639,20 +639,20 @@ def gettreqs():
     classList = list()
     haveTaken = set()
     i = 0
-    similarity = dict()
-    similarStudents = dict()
-    classesTaken = list()
-    for bestTaken in takenAlready:
-        bb = db.session.query(models.Taken).filter(models.Taken.class_id == bestTaken.class_id).all()
-        for b in bb:
-            classesTaken.append(b)
-    for eachClass in classesTaken:
-        studentsWithSameClasses = db.session.query(models.Taken).filter(models.Taken.class_id == eachClass.class_id).all()
-        for student in studentsWithSameClasses:
-            if student.student_id not in similarStudents:
-                similarStudents[student.student_id] = 0
-            similarStudents[student.student_id]+=(student.star_number-3)*(eachClass.star_number-3)
-    bestSimilar = sorted(similarStudents.items(), key=lambda x: x[1])[0:5]
+    # similarity = dict()
+    # similarStudents = dict()
+    # classesTaken = list()
+    # for bestTaken in takenAlready:
+    #     bb = db.session.query(models.Taken).filter(models.Taken.class_id == bestTaken.class_id).all()
+    #     for b in bb:
+    #         classesTaken.append(b)
+    # for eachClass in classesTaken:
+    #     studentsWithSameClasses = db.session.query(models.Taken).filter(models.Taken.class_id == eachClass.class_id).all()
+    #     for student in studentsWithSameClasses:
+    #         if student.student_id not in similarStudents:
+    #             similarStudents[student.student_id] = 0
+    #         similarStudents[student.student_id]+=(student.star_number-3)*(eachClass.star_number-3)
+    # bestSimilar = sorted(similarStudents.items(), key=lambda x: x[1])[0:5]
 
     for _,eachClass in enumerate(classesWithReqs.keys()):
         if eachClass.class_id in haveTaken:
